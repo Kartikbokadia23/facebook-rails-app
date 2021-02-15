0835586f6a09db752aa665f925d47057cf87f96a
kartikbokadia23 <kartikbokadia@gmail.com>
2021-02-15 17:00:26 +0530

    added associations between all models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :posts
   devise_for :users
+
+  root to: 'posts#index'
+  resources :friend_requests, only: [:create, :update, :destroy]
+  resources :posts do
+    resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
