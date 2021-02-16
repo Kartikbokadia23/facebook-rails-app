00c7a27214708859c3e013f40ce0aaeb31d314eb
kartikbokadia23 <kartikbokadia@gmail.com>
2021-02-16 17:40:00 +0530

    configured mailer for signup welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,6 @@
 class UserMailer < ApplicationMailer
+    def welcome_email(user)
+        @user = user
+        mail(to: @user.email, subject: 'You are signed up for odin Facebook')
+      end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,11 +22,19 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+
+  after_create :send_welcome_email
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
     end
+  end
+
+  def send_welcome_email
+    UserMailer.welcome_email(self).deliver_now
+    #UserMailer.with(user: @user).welcome_email.deliver_now
   end
 end
