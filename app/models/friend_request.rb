204b3cfa6d858e2fcc234161f253268325f010f1
kartikbokadia23 <kartikbokadia@gmail.com>
2021-02-16 12:12:08 +0530

    resolved incoming friend request with requester_id

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -2,7 +2,7 @@ class FriendRequest < ApplicationRecord
     belongs_to :requester, class_name: 'User'
     belongs_to :requestee, class_name: 'User'
   
-    #validates :requestee, uniqueness: { scope: :requester }
+    validates :requestee, uniqueness: { scope: :requester }
   
     def accept
       requestee.friends << requester
